e1a67dc82bb06b638d16aa407ac69538c37a725c
Lei-Xixia <1377158174@qq.com>
2022-04-24 22:33:14 +0800

    feat: axios二次封装

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -2,7 +2,6 @@
   <h3>登录页面</h3>
   <!-- <el-button>回首页</el-button> -->
   <!-- <router-link to="/welcome">回 首页</router-link> -->
-  <Welcome msg="jack" />
   <el-button @click="goHome">回首页</el-button>
 </template>
 
@@ -10,8 +9,24 @@
 import Welcome from "./Welcome.vue";
 export default {
   name: "Login",
-  components: {
-    Welcome,
+  // components: {
+  //   Welcome,
+  // },
+  mounted() {
+    // this.$request({
+    //   methods: 'get',
+    //   url: '/login',
+    //   data: {
+    //     name:'leilei'
+    //   }
+    // }).then((res) => {
+    //   console.log(res);
+    // })
+    this.$request
+      .get("/login", { name: "leieli" }, { mock: true, loading: true })
+      .then((res) => {
+        console.log(res);
+      });
   },
   methods: {
     goHome() {
--- src/main.js
+++ src/main.js
@@ -3,14 +3,9 @@ import App from './App.vue'
 import router from './router'
 import ElementPlus from "element-plus"
 import 'element-plus/dist/index.css'
-import axios from 'axios'
-import config from './config'
-axios.get(config.mockApi + '/login').then((res)=>{
-  console.log(res);
-})
-
-console.log(import.meta.env);
+import request from './utils/request'
 
 
 const app = createApp(App)
+app.config.globalProperties.$request = request
 app.use(router).use(ElementPlus).mount('#app')
